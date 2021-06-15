94e9d66a68ede26e56c795b83948328c81749aee
andredantas00 <andre.dantas@softvaro.com.br>
2021-06-15 08:56:32 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {
+	SmlsModal
+} from '@smiles/smiles-ui-kit-react';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SmlsModal
+					isOpen={true}
+					type="right"
+					//toggle={() => setModalIsOpen(false)}
+					//onExit={() => setModalIsOpen(false)}
+					className="modal-password"
+				>
+
+					<h4 id="lbl_titlePasswordAlert">Teste Modal</h4>
+
+
+				</SmlsModal>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
